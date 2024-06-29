a62bec1a5d5b4f10ff31c4cb8844d342e9a4d2f9
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-06-29 18:50:07 +0700

    Made the game playable from the front end, added ability to set player names.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,36 +1,34 @@
 const gameboard = (function() {
-    const gameboardArray = new Array(9);
-    return {gameboardArray};
+    let gameboardArray = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+
+    const clearArray = () => {
+        for (let i = 0; i < 9; i++) {
+            gameboardArray[i] = undefined;
+        }
+
+        populateSquares();
+    }
+
+    return {gameboardArray, clearArray};
 })();
 
 const gameControl = (function() {
     let turn = 1;
+    let stop = false;
+    const getStop = () => stop;
 
-    const getPlayerChoice = function() {
-        let choice = prompt(`Player ${turn}, choose a square (1-9).`);
-    
-        if (choice < 1 || choice > 9) {
-            console.log(`Try again. Player ${turn}, choose a square (1-9).`);
-            getPlayerChoice();
-        } else {
-            play(choice - 1);
-        }
-    }
+    const getPlayerChoice = function(choice) {
 
-    const play = function(choice) {
         if (gameboard.gameboardArray[choice] == undefined && turn == 1) {
             gameboard.gameboardArray[choice] = 1;
             switchPlayer();
-            console.log(gameboard.gameboardArray);
         } else if (gameboard.gameboardArray[choice] == undefined && turn == 2) {
             gameboard.gameboardArray[choice] = 2;
             switchPlayer();
-            console.log(gameboard.gameboardArray);
         } else {
-            console.log(`Square is not free. Try again. Player ${turn}, choose a square (1-9).`);
-            getPlayerChoice();
+            message.innerHTML = `Square is not free. Try again. Player ${turn}, choose a square.`;
         }
-        
+
         if ((gameboard.gameboardArray[0] == 1 && gameboard.gameboardArray[1] == 1 && gameboard.gameboardArray[2] == 1)
             || (gameboard.gameboardArray[3] == 1 && gameboard.gameboardArray[4] == 1 && gameboard.gameboardArray[5] == 1)
             || (gameboard.gameboardArray[6] == 1 && gameboard.gameboardArray[7] == 1 && gameboard.gameboardArray[8] == 1)
@@ -39,7 +37,8 @@ const gameControl = (function() {
             || (gameboard.gameboardArray[2] == 1 && gameboard.gameboardArray[5] == 1 && gameboard.gameboardArray[8] == 1)
             || (gameboard.gameboardArray[0] == 1 && gameboard.gameboardArray[4] == 1 && gameboard.gameboardArray[8] == 1)
             || (gameboard.gameboardArray[2] == 1 && gameboard.gameboardArray[4] == 1 && gameboard.gameboardArray[6] == 1)) {
-            return console.log('Player 1 wins!');
+            message.innerHTML = `${player1.playerName} wins!`;
+            stop = true;
         } else if ((gameboard.gameboardArray[0] == 2 && gameboard.gameboardArray[1] == 2 && gameboard.gameboardArray[2] == 2)
             || (gameboard.gameboardArray[3] == 2 && gameboard.gameboardArray[4] == 2 && gameboard.gameboardArray[5] == 2)
             || (gameboard.gameboardArray[6] == 2 && gameboard.gameboardArray[7] == 2 && gameboard.gameboardArray[8] == 2)
@@ -48,29 +47,91 @@ const gameControl = (function() {
             || (gameboard.gameboardArray[2] == 2 && gameboard.gameboardArray[5] == 2 && gameboard.gameboardArray[8] == 2)
             || (gameboard.gameboardArray[0] == 2 && gameboard.gameboardArray[4] == 2 && gameboard.gameboardArray[8] == 2)
             || (gameboard.gameboardArray[2] == 2 && gameboard.gameboardArray[4] == 2 && gameboard.gameboardArray[6] == 2)) {
-            return console.log('Player 2 wins!');
+            message.innerHTML = `${player2.playerName} wins!`;
+            stop = true;
         } else if (gameboard.gameboardArray.findIndex(each => each == undefined) == -1) {
-            return console.log('A tie!');
-        } else {
-            getPlayerChoice();
+            message.innerHTML = 'A tie!';
+            stop = true;
         }
+
+        populateSquares();
     }
 
     const switchPlayer = function() {
         if (turn == 1) {
             turn = 2;
-            return 1;
+            message.innerHTML = `${player2.playerName}, choose a square.`;
         } else {
             turn = 1;
-            return 2;
+            message.innerHTML = `${player1.playerName}, choose a square.`;
         }
     }
 
-    return {getPlayerChoice};
+    return {turn, getStop, getPlayerChoice};
 })();
 
-function createPlayer(name, sign) {
+function createPlayer(name) {
+    const playerName = name;
 
+    return {playerName};
 }
 
-gameControl.getPlayerChoice();
+let player1 = createPlayer('Player 1');
+let player2 = createPlayer('Player 2');
+
+const allSquares = document.querySelectorAll('.button');
+const form = document.querySelector('#form');
+const restart = document.querySelector('#restart');
+const message = document.querySelector('#message');
+
+function populateSquares() {
+    allSquares.forEach(button => {
+        if (gameboard.gameboardArray[button.id - 1] == 1) {
+            button.innerHTML = 'X';
+        } else if (gameboard.gameboardArray[button.id - 1] == 2) {
+            button.innerHTML = 'O';
+        } else {
+            button.innerHTML = '';
+        }
+    });
+}
+
+populateSquares();
+
+allSquares.forEach(button => {
+    button.addEventListener('mouseover', changeColorIn.bind(this, button));
+    button.addEventListener('mouseout', changeColorOut.bind(this, button));
+    button.addEventListener('click', clickSquare.bind(this, button));
+});
+
+function changeColorIn(button) {
+    button.style.filter = 'brightness(90%)';
+}
+
+function changeColorOut(button) {
+    button.style.filter = 'brightness(100%)';
+}
+
+function clickSquare(button) {
+    if (gameControl.getStop()) {
+    } else {
+        gameControl.getPlayerChoice(button.id - 1);
+    }
+}
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+    player1.playerName = document.querySelector('#player1').value;
+    player2.playerName = document.querySelector('#player2').value;
+
+    if (gameControl.turn == 1) {
+        message.innerHTML = `${player1.playerName}, choose a square.`;
+    } else {
+        message.innerHTML = `${player2.playerName}, choose a square.`;
+    }
+
+    event.preventDefault();
+}
+
+restart.addEventListener('click', gameboard.clearArray);
